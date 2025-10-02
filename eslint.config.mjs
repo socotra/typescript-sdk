968b951795d46ab4485193c494206ecb85330b72
Konstantin Konstantinov <KKonstantinov@users.noreply.github.com>
2025-10-02 20:16:28 +0300

    MCP SDK: Add Prettier to Typescript SDK (#976)
    Co-authored-by: ihrpr <inna@anthropic.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default tseslint.config(
     eslint.configs.recommended,
@@ -22,5 +23,6 @@ export default tseslint.config(
         rules: {
             "no-console": "error"
         }
-    }
+    },
+    eslintConfigPrettier
 );
